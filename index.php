082f5c33489ef3830942c647a1a503893fefe6be
Slavomir Kozar <slavo.kozar@gmail.com>
2017-10-13 02:40:35 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    namespace blackjack;
+
+    require('card.php');
+
+    $suits = ['heart', 'spade', 'club', 'diamond'];
+    $values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
+
+    $deck = [];
+    foreach ($suits as $suit) {
+        foreach ($values as $value) {
+
+            $deck[] = new card($suit, $value);
+        }
+    }
+
+    shuffle($deck);
+
+?>
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+
+<div id="deck">
+    <?php
+    foreach($deck as $card){
+        echo $card;
+    }
+    ?>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,26 @@
+# BlackJack PHP deck
+
+This project is simple implementation of deck of 52 french cards. 
+
+There are 4 color of cards: **heart, spade, club, diamond,**
+13 values:
+**2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A**
+
+Each card is interpreted by class **Card** stored in card.php.
+ 
+Class **Card** has attributes ``suit, value`` and method ``toString`` which returns element implementing single card in deck. 
+
+```html
+<div class="card" data-suit="spade" data-value="A">
+</div>
+```
+
+The ``data`` attribute of every tag can be easily read by jQuery. 
+ In order to read attribute ``data-my_value`` there is ``$(selector).data('my_value')`` function.
+ 
+ In order to read value and suite of our card:
+
+```javascript
+var value = $('.card').data('value');
+var suite = $('.card').data('suite');
+```
